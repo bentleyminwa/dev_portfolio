d378734040683d6a9f4c9405cfb90d1084be53b0
bentleyminwa <bentleyminwa@gmail.com>
2025-01-30 16:19:45 +0300

    feat: replace Link with button in Banner component for navigation

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,6 +1,8 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const Banner = () => {
+  const navigate = useNavigate();
+
   return (
     <section className="bg-gray-100 px-8 py-10">
       <h1 className="text-4xl mb-5">Get in Touch!</h1>
@@ -9,12 +11,12 @@ const Banner = () => {
         ideas, ask your questions, or just say hello. Let’s talk and bring
         something amazing to life together!
       </p>
-      <Link
-        to={"contact"}
-        className="bg-gray-900 text-gray-50 uppercase p-4 tracking-widest mb-5"
+      <button
+        onClick={() => navigate("/contact")}
+        className="bg-gray-900 text-gray-50 uppercase p-4 tracking-widest mb-5 cursor-pointer"
       >
         Contact Me!
-      </Link>
+      </button>
     </section>
   );
 };
